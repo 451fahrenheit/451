ddbf02f8426a6117962e3aa1e7fe05a70e61eaf5
nitin <nitin@incubyte.co>
2022-11-03 19:48:28 +0530

    ADD 1. registerpage 2. react router dom

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
+/* eslint-disable import/no-unresolved */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './index.css';
-import App from './App';
+
+import Register from './pages/Register';
 import reportWebVitals from './reportWebVitals';
 
+export default function App(){
+	return(
+		<BrowserRouter>
+			<Routes>
+				<Route path="register" element={<Register />}/>
+			</Routes>
+		</BrowserRouter>
+	);
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
