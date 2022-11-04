dfd3eed75736a3fb1a21e414f3024a07cecfcaa4
nitin <nitin@incubyte.co>
2022-11-04 22:22:29 +0530

    ADD 1. Configured Chakra UI 2. Added register form

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import './index.css';
 
 import Register from './pages/Register';
-import reportWebVitals from './reportWebVitals';
 
 export default function App(){
 	return(
@@ -20,11 +20,9 @@ export default function App(){
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		<ChakraProvider>			
+			<App />
+		</ChakraProvider>
 	</React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
