e2753c9c4e4e98946bfc96372b69ea1da4276711
nitin <nitin@incubyte.co>
2022-11-16 19:21:29 +0530

    ADD changed Register to Account  Added login test

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-unresolved */
 import React from 'react';
-import Register from '../components/Register';
+import Account from '../components/Account';
 
-const RegisterPage = () => <Register />;
+const RegisterPage = () => <Account />;
 
 export default RegisterPage;
