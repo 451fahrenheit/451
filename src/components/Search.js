59c45a160598121e0fc3d2555aa6448381ea45e3
nitin <nitin@incubyte.co>
2022-11-18 23:56:08 +0530

    ADD Search text and get results Need to refactor a lot

--- src/components/Search.js
+++ src/components/Search.js
@@ -11,69 +11,55 @@ import {
 } from '@chakra-ui/react';
 import { useState } from 'react';
 import Navbar from './Navbar';
-import {gql, useMutation} from '@apollo/client';
-const SEARCH_MUTATION = gql`
-mutation CreateUser($email: String!, $password: String!){
-	createUser(input: {authProvider: {credentials: {email: $email, password: $password}}}) {
-		user {
-			id
-			email
-		
+import { gql, useLazyQuery} from '@apollo/client';
+
+const SEARCH_QUERY = gql `
+query Titles($search_title: String!) {
+	titles(searchTitle:$search_title){
+		volumeId
+		title
+		subtitle
+		description
+		authors
+		language
+		pubDate
+		smallthumbnail
+		thumbnail
 	}
-}				
-}
-	`;
+	}
+`;
+
 function Search(){
 	const [searchText,setSearchText] = useState('');
 	const [errorMessage,setError] = useState('');
+	const [books,setBooks] = useState([]);
 
-	const [searchTitle,{loading, data}] = useMutation(SEARCH_MUTATION, {
+	const [search] = useLazyQuery(SEARCH_QUERY, {
 		variables: {
-			searchText: searchText,
+			search_title: searchText,
+		},
+		onCompleted: (data) => {
+			if(data.titles)
+			{
+				setBooks(data.titles);
+			}
+			else{
+				setError('Something went wrong, please try again');
+			}
+
 		},
 		onError: (error) => {
 			setError(error.message);
-		}		
+		}
 	});
+
 	function handleSearch(){
-		searchTitle();
+		search();
 	}
 	function handleSearchText(e){
 		setSearchText(e.target.value);
 	}
-	const bookCard = <Box
-		rounded={'lg'}
-		bg={useColorModeValue('white', 'gray.700')}
-		boxShadow={'lg'}
-		minHeight={100}
-		minWidth={480}>
-		<HStack>
-			<Box>
-				<Image maxHeight={90} maxWidth={90} m={2} src='https://bit.ly/dan-abramov' alt='Dan Abramov'></Image>
-			</Box>
-			<VStack align="left" paddingRight={10}>
-				<Text >
-					Title
-				</Text>
-				<Text>
-					Author
-				</Text>
-				<HStack>
-					<Text>Language</Text>
-					<Text>Published on</Text>
-				</HStack>
-			</VStack>
-			<VStack align="right" >
-				<Box>
-					<Button>Add to library</Button>
-				</Box>
-				<Box>
-					<Button>Buy</Button>
-				</Box>
-			</VStack>
-		</HStack>
 
-	</Box>;
 	return (<>
 		<Navbar/>
 		<Flex minH={'100vh'} align="top" justify="center" bg={useColorModeValue('gray.50', 'gray.800')} pt={5}>
@@ -92,9 +78,43 @@ function Search(){
 					
 				</Box>
 				<p>{errorMessage}</p>
-				if(!loading){
-					{bookCard}
-				}	
+				<div>
+					{books.length>0?books.map(book=>
+						<div key={book.volumeId}>	<Box
+							rounded={'lg'}
+							bg={useColorModeValue('white', 'gray.700')}
+							boxShadow={'lg'}
+							minHeight={100}
+							minWidth={480}
+							mb={1}>
+							<HStack>
+								<Box>
+									<Image minHeight={90} minWidth={90} maxHeight={90} maxWidth={90} m={2} src={book.smallthumbnail} alt='Dan Abramov'></Image>
+								</Box>
+								<VStack align="left" paddingRight={10}>
+									<Text maxWidth={40}>
+										{book.title}
+									</Text>
+									<Text>
+									by {book.authors.join(',')}
+									</Text>
+									<HStack>
+										<Text>{book.language}</Text>
+										<Text>{book.pubDate}</Text>
+									</HStack>
+								</VStack>
+								<VStack align="right" >
+									<Box>
+										<Button>Add to library</Button>
+									</Box>
+									<Box>
+										<Button>Buy</Button>
+									</Box>
+								</VStack>
+							</HStack>
+			
+						</Box></div>):''};
+				</div>
 			</VStack>
 
 
